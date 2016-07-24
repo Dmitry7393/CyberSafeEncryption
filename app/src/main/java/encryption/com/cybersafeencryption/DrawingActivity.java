18d6ca4349fb4cf276a0ba4cabf75e5f8f36cf9d
Dmitry7393 <dmitry7393@gmail.com>
2016-07-24 16:34:11 +0300

    Added RelativeLayout in activity_drawing xml

--- app/src/main/java/encryption/com/cybersafeencryption/DrawingActivity.java
+++ app/src/main/java/encryption/com/cybersafeencryption/DrawingActivity.java
@@ -14,31 +14,31 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
 
 
 public class DrawingActivity extends AppCompatActivity {
-    FrameLayout holder;
+    RelativeLayout holder;
     CanvasView canvasView;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawing);
         canvasView = (CanvasView) findViewById(R.id.signature_canvas);
-        holder = (FrameLayout) findViewById(R.id.frm_layout);
+        holder = (RelativeLayout) findViewById(R.id.layout_canvas);
     }
     public void clearCanvas(View v) {
         canvasView.clearCanvas();
     }
     public void saveImage(View v) {
-        String fileName = Environment.getExternalStorageDirectory() + "/test6.png";
+        String fileName = Environment.getExternalStorageDirectory() + "/myimage.png";
         try {
 
             OutputStream stream = new FileOutputStream(fileName);
             Bitmap myBitmap = loadBitmapFromView(holder);
             myBitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
             stream.close();
-            Log.d("SUCCESS12345", "TTTT234uuuu3");
+            Log.d("SUCCESS123458", "TTTT234uuuu3");
         }
         catch(FileNotFoundException e) {
             Log.d("ERROR1", "TTTT");
@@ -52,7 +52,7 @@ public class DrawingActivity extends AppCompatActivity {
         v.measure(View.MeasureSpec.makeMeasureSpec(dm.widthPixels, View.MeasureSpec.EXACTLY),
                 View.MeasureSpec.makeMeasureSpec(dm.heightPixels, View.MeasureSpec.EXACTLY));
         v.layout(0, 0, v.getMeasuredWidth(), v.getMeasuredHeight());
-        ((FrameLayout) v).setForegroundGravity(Gravity.CENTER); //setGravity(Gravity.CENTER);
+        ((RelativeLayout) v).setGravity(Gravity.CENTER);
         Bitmap returnedBitmap = Bitmap.createBitmap(v.getMeasuredWidth(),
                 v.getMeasuredHeight(), Bitmap.Config.ARGB_8888);
         Canvas c = new Canvas(returnedBitmap);
